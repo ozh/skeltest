2ed7085d17c80d6854603c824ffeb1e47bdcf0be
ozh <ozh@ozh.org>
2017-06-30 19:33:13 +0200

    \PHPUnit\Framework\TestCase

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -2,7 +2,7 @@
 
 namespace ozh\skeltest;
 
-class ExampleTest extends PHPUnit\Framework\TestCase
+class ExampleTest extends \PHPUnit\Framework\TestCase
 {
     /**
      * Test that true does in fact equal true
